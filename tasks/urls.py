8c026a883267e8af3ae988cb5364ad1b4c52d354
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-19 14:24:27 -0300

    create delete task url path

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_tasks, list_task, create_task, update_task
+from .views import list_tasks, list_task, create_task, update_task, delete_task
 
 urlpatterns = [
     path('', list_tasks, name='list-tasks'),
     path('list-task/<str:pk>/', list_task, name='list-task'),
     path('create-task/', create_task, name='create-task'),
-    path('update-task/<str:pk>/', update_task, name='update-task')
+    path('update-task/<str:pk>/', update_task, name='update-task'),
+    path('delete-task/<str:pk>/', delete_task, name='delete-task')
 ]
