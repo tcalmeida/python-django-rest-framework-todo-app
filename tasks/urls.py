34f13640468c1d4c211445851df4e9003e837f7b
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-19 12:18:28 -0300

    create list one task url path

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import list_tasks
+from .views import list_tasks, list_task
 
 urlpatterns = [
     path('', list_tasks, name='list-tasks'),
-
+    path('/<str:pk>', list_task, name='list-task'),
 ]
