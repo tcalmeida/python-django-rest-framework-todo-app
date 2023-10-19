9216b61d9ffcade10738ca407ff445d7bdcc7597
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-19 12:09:00 -0300

    create list all tasks view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def list_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+
