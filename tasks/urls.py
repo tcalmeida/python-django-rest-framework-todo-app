5e999a6ebe9925e71630ff226322853d447d9eee
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-19 14:48:39 -0300

    refactor: change list tasks url path

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import list_tasks, list_task, create_task, update_task, delete_task
 
 urlpatterns = [
-    path('', list_tasks, name='list-tasks'),
+    path('list-tasks', list_tasks, name='list-tasks'),
     path('list-task/<str:pk>/', list_task, name='list-task'),
     path('create-task/', create_task, name='create-task'),
     path('update-task/<str:pk>/', update_task, name='update-task'),
